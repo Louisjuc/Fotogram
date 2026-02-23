1c268956efa2f24b0d50bb130c02027eea57b820
louisj <louis.juchem@web.de>
2026-02-23 11:39:21 +0100

    small changes

--- js/script.js
+++ js/script.js
@@ -61,18 +61,20 @@ function imgTemplate(index){
     return`
 
     <img src="./img/${imgData[index]}" class="gallery_img" onclick="openDialog(${index})" alt="${ImgDescription[index]}"></img>
-    <dialog class="imgDialog">
+    <dialog class="imgDialog" aria-haspopup="dialog" aria-control="Image Dialog">
     <section class="dialog_section">
     <div class="menu_flex">
     <p>${ImgDescription[index]}</p>
-    <img src="./img/close.svg" onclick="closeDialog(${index})" class="close_button" alt="Button zum Schließen" tabeindex="0">
+    <button onclick="closeDialog(${index})" class="close_button">
+    <img src="./img/close.svg"  alt="Button zum Schließen">
+    </button>
     </div>
     <img src="./img/${imgData[index]}" class="img_full-width" alt="${ImgDescription[index]}">
     </section>
     <nav>
-  <button onclick="prevPicture(${index})">  <img src="./img/prev.svg" alt="Pfeil zurück" > </button>
+  <button onclick="prevPicture(${index})" >  <img src="./img/prev.svg" alt="Pfeil zurück" > </button>
     <p>${ImgNumber[index]}/12</p>
-   <button onclick="nextPicture(${index})"> <img src="./img/next.svg" alt="Vorwärts"></button>
+   <button onclick="nextPicture(${index})" aria-label="Button nach Vorne"> <img src="./img/next.svg" alt="Vorwärts"></button>
 
     </nav>
   </dialog>
